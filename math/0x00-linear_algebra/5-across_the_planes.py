9f38ea3a91ededa177dd245e53f1b25d47a180bb
anaruzz <laabidigh@gmail.com>
2020-08-06 03:47:59 +0100

    across the planes

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
-matrix_shape = __import__('2-size_me_please').matrix_shape
+"""
+Script that adds two matrices element wise
+"""
+
 
 def add_matrices2D(mat1, mat2):
-    if matrix_shape(mat1) == matrix_shape(mat2):
+    """
+    function that adds two matrices element wise
+    """
+    s = len(mat1)
+    if s - len(mat2) != 0 or len(mat1[0]) - len(mat2[0]) != 0:
+        return None
+    else:
         new = []
-        for i in range(len(mat1)):
+        j = 0
+        for i in range(s):
             line = []
-            for j in range(a):
-                b = int(mat1[i][j]) + int(mat2[i][j])
-                line.append(b)
+            for j in range(len(mat1[0])):
+                line.append(mat1[i][j] + mat2[i][j])
             new.append(line)
         return new
-    else:
-        return None
