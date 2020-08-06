a2ec359c6b6a74486cf24d3daec17684643bae50
anaruzz <laabidigh@gmail.com>
2020-08-06 02:50:10 +0100

    size me up please

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -7,8 +7,15 @@ def matrix_shape(matrix):
     Function that calculates the shape
     of a matrix
     """
+    # size is the length of the lines in the matrix
     size = [len(matrix)]
     while type(matrix[0]) is list:
+        """
+        while the matrix contains an other matrix enter
+        the while loop
+        and then each time add the number of elements
+        in that line in the certain dimension
+        """
         size.append(len(matrix[0]))
         matrix[0] = matrix[0][0]
     return size
