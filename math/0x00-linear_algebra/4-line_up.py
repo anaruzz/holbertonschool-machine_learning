f323ad62835a5b1a15b63618dc4c783f1bbcf1e5
anaruzz <laabidigh@gmail.com>
2020-08-06 02:57:42 +0100

    line up

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
+"""
+Script that adds two arrays element-wise
+"""
+
 
 def add_arrays(arr1, arr2):
+    """
+    function that adds two arrays element-wise
+    """
     if len(arr1) == len(arr2):
         new = []
         for i in range(len(arr1)):
