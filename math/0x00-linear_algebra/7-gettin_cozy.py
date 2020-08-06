dd40b4b4015ed1794c2d008876ff6e08a19211f3
anaruzz <laabidigh@gmail.com>
2020-08-06 04:34:31 +0100

    ok

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -8,12 +8,11 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """
     function that concatenates two matrices
     """
+    m = []
     if axis == 0 and len(mat1[0]) == len(mat2[0]):
-            return mat1 + mat2
+        return mat1+mat2
     elif axis == 1 and len(mat1) == len(mat2):
-            new = []
-            for i in range(len(mat1)):
-                new.append(mat1[i] + mat2[i])
-            return(new)
-    else:
-            return(None)
+        for i in range(len(mat1)):
+            l1 = mat1[i]+mat2[i]
+            m.append(l1)
+        return m
