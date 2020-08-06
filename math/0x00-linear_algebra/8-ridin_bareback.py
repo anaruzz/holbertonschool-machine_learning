ab94dbdca0de59b61b431543e0518d6c99e085b4
anaruzz <laabidigh@gmail.com>
2020-08-06 05:33:48 +0100

    ridin' barback

---
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Script that multiplies two matrices
+"""
+
+
+def mat_mul(mat1, mat2):
+    """
+    function that multiplies two matrices
+    """
+
+    lines1 = len(mat1)
+    rows1 = len(mat1[0])
+    lines2 = len(mat2)
+    rows2 = len(mat2[0])
+
+    if lines1 != rows2 and lines2 != rows1:
+        return None
+    elif lines1 == rows2:
+        new = [[0 for i in range(rows1)] for j in range(lines2)]
+    else:
+        new = [[0 for i in range(rows2)] for j in range(lines1)]
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat2)):
+                new[i][j] += mat1[i][k] * mat2[k][j]
+    return new
