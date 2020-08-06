c9a29917096f5e1065098dd1fded5b50c24ba6b2
anaruzz <laabidigh@gmail.com>
2020-08-06 10:49:56 +0100

    ill use my scale

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Script that concatenates two matrices
+"""
+import numpy as np
+
+
+def np_shape(matrix):
+    """
+    function that concatenates two matrices
+    """
+    return matrix.shape
