ff6ff7f128d10693eb212e2b5ceac1aab4199be9
anaruzz <laabidigh@gmail.com>
2020-08-06 02:55:04 +0100

    flip me over

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
+"""
+Script that returns the transpose of a 2D matrix
+"""
 def matrix_transpose(matrix):
+    """
+    function that returns
+    the transpose of a 2D matrix,
+    """
     i = 0
     new = [[0 for i in range(len(matrix))] for j in range(len(matrix[i]))]
     for i in range(len(matrix)):
