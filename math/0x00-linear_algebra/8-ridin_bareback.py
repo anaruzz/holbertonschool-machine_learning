b4e4e3dc18e17ba3334a6d4a2138b0e65c4a18f1
anaruzz <laabidigh@gmail.com>
2020-08-06 05:56:40 +0100

    fix

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -12,12 +12,16 @@ def mat_mul(mat1, mat2):
     lines1, lines2 = len(mat1), len(mat2)
     rows1, rows2 = len(mat1[0]), len(mat2[0])
 
-    if lines1 == rows2:
-        new = [[0 for i in range(rows2)] for j in range(lines1)]
+    if lines1 == rows2 or lines2 == rows1:
+        new = []
         for i in range(len(mat1)):
+            t = []
             for j in range(len(mat2[0])):
+                s = 0
                 for k in range(len(mat2)):
-                    new[i][j] += mat1[i][k] * mat2[k][j]
+                    s += mat1[i][k] * mat2[k][j]
+                t.append(s)
+            new.append(t)
         return new
     else:
         return None
