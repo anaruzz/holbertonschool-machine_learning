648ea68ca0640b39270b82382353224508ce771c
anaruzz <laabidigh@gmail.com>
2020-08-06 04:09:29 +0100

    howdy partner

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Script that adds two lists
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    function that adds two lists
+    """
+    new = arr1 + arr2
+    return new
