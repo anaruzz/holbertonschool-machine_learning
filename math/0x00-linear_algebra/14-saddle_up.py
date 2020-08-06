5f5fc78b882a8a0b90237879182c830e31b8323d
anaruzz <laabidigh@gmail.com>
2020-08-06 11:20:56 +0100

    saddle up

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Script that multiplies two arrays
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    function that multiplies two arrays
+    """
+    return mat1 @ mat2
