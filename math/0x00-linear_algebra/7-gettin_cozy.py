0bc10f76d7e14ae960b975e80a04e89a4c0d510c
anaruzz <laabidigh@gmail.com>
2020-08-06 04:27:41 +0100

    gettin' cozy

---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Script that adds two matrices element wise
+"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """
+    function that adds two matrices element wise
+    """
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
+            m = mat1 + mat2
+            return(m)
+    elif axis == 1 and len(mat1) == len(mat2):
+            n = []
+            for i in range(len(mat1)):
+                n.append(mat1[i] + mat2[i])
+            return(n)
+    else:
+            return(None)
