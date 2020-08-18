44cf4b9507c1413e6dadd94d30c298fff379c017
anaruzz <laabidigh@gmail.com>
2020-08-18 19:42:07 +0100

    sum

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -3,13 +3,16 @@
  Script that does the summation function of i²
 """
 
+
 def summation_i_squared(n):
     """
     sum if i² from 1 to n
     """
-    if type(n) is not int or n < 1:
+    if type(n) is not int:
         return None
     elif n == 1:
         return 1
+    elif n < 0:
+        return n ** 2 + summation_i_squared(n+1)
     else:
         return n ** 2 + summation_i_squared(n-1)
