7d5e0ea9ee343b6f28ca1182c015119241dc4a30
anaruzz <laabidigh@gmail.com>
2020-08-19 12:27:04 +0100

    matisse

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -6,11 +6,8 @@ Script that calculates the derivative of a polynomial
 
 def poly_derivative(poly):
     new = []
-    new.append(poly[1])
     if type(poly) is not list or len(poly) == 0:
         return None
-    for i in range(len(poly)-1):
+    for i in range(1, len(poly)):
         new.append(i * poly[i])
-    while new[len(new)-1] == 0:
-        new.pop()
     return new
