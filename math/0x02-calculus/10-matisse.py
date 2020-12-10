c8be0ad1bc36638a150bd9e91aa5f2f17d4de040
anaruzz <laabidigh@gmail.com>
2020-12-10 18:36:06 +0100

    ok

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -4,13 +4,13 @@
 
 def poly_derivative(poly):
     """ doc """
-    if len(poly) == 0 or type(poly) is not list:
+    if len(poly) == 0 or type(poly) != list:
         return None
 
     drv = []
     if len(poly) == 1:
         return [0]
     for i in range(1, len(poly)):
-        drv.append(poly[i] * i)
+        drv.append(i * poly[i])
 
     return drv
