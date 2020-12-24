c77ddad2e2f64a0cfc472c7464076355d632e1f1
anaruzz <laabidigh@gmail.com>
2020-12-25 00:11:50 +0100

    cdf for exponential

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -28,3 +28,8 @@ class Exponential():
         if x < 0:
             return 0
         return self.lambtha * 2.7182818285 ** (-self.lambtha * x)
+
+    def cdf(self, x):
+        if x < 0:
+            return 0
+        return 1 - 2.7182818285 ** (-x * self.lambtha)
