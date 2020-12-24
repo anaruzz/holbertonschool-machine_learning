f4555fd97d1a4e919ef56e0ce13f2b2ad45ce23b
anaruzz <laabidigh@gmail.com>
2020-12-24 21:55:11 +0100

    Poisson PMF

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -20,3 +20,13 @@ class Poisson():
             if len(data) < 2:
                 raise ValueError('data must contain multiple values')
             self.lambtha = sum(data)/len(data)
+
+    def pmf(self, k):
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        fct = 1
+        for i in range(1, k+1):
+            fct = fct * i
+        return (self.lambtha ** k * 2.7182818285 ** (- self.lambtha)) / fct
