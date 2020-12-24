fdb258b6288779748ff1add969edec5e675b2f05
anaruzz <laabidigh@gmail.com>
2020-12-25 00:00:06 +0100

    Exponential pdf

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -20,3 +20,8 @@ class Exponential():
             if len(data) <= 2:
                 raise ValueError('data must contain multiple values')
             self.lambtha = len(data) / sum(data)
+
+    def pdf(self, x):
+        if x < 0:
+            return 0
+        return self.lambtha * 2.7182818285 ** (-self.lambtha * x)
