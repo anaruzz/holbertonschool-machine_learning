b23d60ffc035c4267bab133b0ec09ab2c374a981
anaruzz <laabidigh@gmail.com>
2020-12-24 21:28:25 +0100

    Poisson constructor

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Script that represents Poisson distribution
+
+class Poisson:
+    """
+    class Poisson to represent poisson distribution
+    """
+    def __init__(self, data=None, lambtha=1.):
+        """
+        class constructor
+        """
+        if data is None:
+            if lambtha < 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = lambtha
+        else:
+            if type(data) is not list:
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.lambtha = sum(data)/len(data)
