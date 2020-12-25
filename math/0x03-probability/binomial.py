92f194c16e759ece722bafe14c909a10726648a8
anaruzz <laabidigh@gmail.com>
2020-12-25 13:04:36 +0100

    mean = n * p

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -14,16 +14,16 @@ class Binomial():
         """
         if data is None:
             if n < 0:
-                raise ValueError('n must be a positive number')
+                raise ValueError('n must be a positive value')
             if p < 0 or p > 1:
                 raise ValueError('p must be greater than 0 and less than 1')
             self.n = int(n)
             self.p = float(p)
         else:
             if type(data) is not list:
-                raise TypeError('data must bee a list')
-            if len(data) < 2:
-                raise ValueError('data must contain values')
+                raise TypeError('data must be a list')
+            if len(data) <= 2:
+                raise ValueError('data must contain multiple values')
             m = sum(data) / len(data)
             s = 0
             for x in data:
