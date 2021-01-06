9fbcc5b85c1f7fd6d4fed764eb40ad656fc3f3a1
anaruzz <laabidigh@gmail.com>
2021-01-06 22:21:13 +0100

    Neural network

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -24,3 +24,11 @@ class NeuralNetwork():
             raise TypeError('nodes must be an integer')
         if nodes < 1:
             raise ValueError('nodes must be a positive integer')
+        self.nx = nx
+        self.nodes = nodes
+        self.W1 = np.random.randn(nodes, self.nx)
+        self.b1 = np.array([[np.array(0.)]] * nodes)
+        self.A1 = 0
+        self.W2 = np.random.randn(nodes, self.nx)
+        self.b2 = 0
+        self.A2 = 0
