7613a87a6ff057ea9223c95dae5b3f53044f31fc
anaruzz <laabidigh@gmail.com>
2021-01-07 19:46:37 +0100

    deep neural network

---
+++ supervised_learning/0x01-classification/13-neural_network.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+"""
+that defines a neural network
+with one hidden layer performing binary classification
+"""
+import numpy as np
+
+
+class NeuralNetwork():
+    """
+    Class thet defines a neural network
+    with one hidden layer
+    performing binary classification
+    """
+    def __init__(self, nx, nodes):
+        """
+        class constructor
+        """
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(nodes) is not int:
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+        self.nx = nx
+        self.nodes = nodes
+        self.__W1 = np.random.randn(nodes, self.nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
+
+    def sigmoid(self, z):
+        """
+        sigmoid activation fuction
+        """
+        return (1 / (1 + np.exp(-z)))
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neural network
+        """
+        z1 = np.dot(self.__W1, X) + self.__b1
+        self.__A1 = self.sigmoid(z1)
+        z2 = np.dot(self.__W2, self.__A1) + self.__b2
+        self.__A2 = self.sigmoid(z2)
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost of the model with logistic regression
+        """
+        m = Y.shape[1]
+        error = (-Y * np.log(A)) - ((1 - Y) * np.log(1.0000001 - A))
+        return 1 / m * np.sum(error)
+
+    def evaluate(self, X, Y):
+        """
+        Evaluates the neural network's predictions
+        """
+        _, self.__A2 = self.forward_prop(X)
+        cost = self.cost(Y, self.__A2)
+        self.__A2 = np.where(self.__A2 >= 0.5, 1, 0)
+        return (self.__A2, cost)
+
+    def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
+        """
+        Calculates one pass of gradient descent
+        """
+        def gradient_descent(self, X, Y, A, alpha=0.05):
+            """
+            Calculates one pass of gradient descent on the neuron
+            """
+            # Update bias
+            self.__b = self.__b - (alpha * np.mean(A - Y))
+            # Update weight
+            m = Y.shape[1]
+            weight_deriv = np.matmul(X, (A-Y).T) / m
+            self.__W -= alpha * weight_deriv.T
---
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Script that defines a deep neural network
+performing binary classification:
+"""
+import numpy as np
+
+
+class DeepNeuralNetwork():
+    """
+    deep neural network
+    """
+    def __init__(self, nx, layers):
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
+        if not list(map(lambda b: b < 1 and type(b) is int, layers)):
+            raise TypeError("layers must be a list of positive integers")
+        self.nx = nx
+        self.layers = layers
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+        for i in range(self.L):
+            nW = 'W' + str(i + 1)
+            n = layers[i]
+            self.weights[nW] = np.random.normal(size=(n, layers[i - 1]))
+            nb = 'b' + str(i + 1)
+            self.weights[nb] = np.zeros((n, 1))
