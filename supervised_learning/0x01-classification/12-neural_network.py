c6d7aa96ac1de08001c191871d544c4546e2c2b4
anaruzz <laabidigh@gmail.com>
2021-01-10 22:47:31 +0100

    try

--- supervised_learning/0x01-classification/12-neural_network.py
+++ supervised_learning/0x01-classification/12-neural_network.py
@@ -85,7 +85,7 @@ class NeuralNetwork():
         """
         Evaluates the neural network's predictions
         """
-        self.forward_prop(X)
-        cost = self.cost(Y, self.A2)
-        prediction = np.where(self.__A2 >= 0.5, 1, 0)
-        return (prediction, cost)
+        _, self.__A2 = self.forward_prop(X)
+        cost = self.cost(Y, self.__A2)
+        self.__A2 = np.where(self.__A2 >= 0.5, 1, 0)
+        return (self.__A2, cost)
