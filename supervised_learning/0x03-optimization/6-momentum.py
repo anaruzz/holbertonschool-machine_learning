54d9f34ffb4fb70f8d404b2d8afb7ed620278757
anaruzz <laabidigh@gmail.com>
2021-01-28 16:23:49 +0100

    momentum Gradient descent

---
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Script that creates the training operation for a
+neural network in tensorflow using the gradient
+descent with momentum optimization algorithm
+"""
+import tensorflow as tf
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """
+    returns the momentum optimization operation
+    """
+    gd = tf.train.MomentumOptimizer(alpha, beta1)
+    return gd.minimize(loss)
