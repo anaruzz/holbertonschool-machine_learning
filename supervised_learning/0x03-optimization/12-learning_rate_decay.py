ed8a5f12458cb69d90920433d81b7b12f75a2ec1
anaruzz <laabidigh@gmail.com>
2021-01-29 15:04:54 +0100

    pep8 fix

--- supervised_learning/0x03-optimization/12-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -10,5 +10,6 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """
     returns the learning rate decay operation
     """
-    gd = tf.train.inverse_time_decay(alpha, global_step, decay_step, decay_rate, staircase=True)
+    gd = tf.train.inverse_time_decay(alpha, global_step, decay_step,
+                                     decay_rate, staircase=True)
     return gd
