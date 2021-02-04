1d492cbaf6342778096da12d4e8363dac1a0201a
anaruzz <laabidigh@gmail.com>
2021-02-04 23:05:09 +0100

    learning rate decay

---
+++ supervised_learning/0x06-keras/7-train.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Script that trains a model with learning rate decay
+"""
+import tensorflow.keras as k
+
+
+def train_model(network, data, labels,
+                batch_size, epochs,
+                validation_data=None,
+                early_stopping=False,
+                patience=0,
+                learning_rate_decay=False,
+                alpha=0.1,
+                decay_rate=1,
+                verbose=True,
+                shuffle=False):
+    """
+    Returns the history object generated after training
+    """
+    callback = []
+    if learning_rate_decay:
+        def LRD(step):
+            return alpha / (1 + decay_rate * step)
+    if early_stopping:
+        early_s = k.callbacks.EarlyStopping(monitor='val_loss',
+                                            patience=patience,
+                                            verbose=verbose)
+        callback.append(early_s)
+    if learning_rate_decay and validation_data:
+        decay = k.callbacks.LearningRateScheduler(LRD, verbose=1)
+        callback.append(decay)
+    history = network.fit(x=data,
+                          y=labels,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          verbose=verbose,
+                          validation_data=validation_data,
+                          shuffle=shuffle,
+                          callbacks=callback
+                          )
+    return history
