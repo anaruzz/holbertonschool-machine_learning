d5e2f2769c243abab8824cd4b2be295b93837702
anaruzz <laabidigh@gmail.com>
2021-02-04 21:22:03 +0100

    fix

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -15,10 +15,10 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     for i in range(len(layers)):
         if i == 0:
             output = (k.layers.Dense(layers[i], activation=activations[i],
-                 kernel_regularizer=k.regularizers.l2(lambtha)))(input)
+                      kernel_regularizer=k.regularizers.l2(lambtha)))(input)
         else:
             output = (k.layers.Dense(layers[i], activation=activations[i],
-                 kernel_regularizer=k.regularizers.l2(lambtha)))(output)
+                      kernel_regularizer=k.regularizers.l2(lambtha)))(output)
         if i != len(layers) - 1:
             output = (k.layers.Dropout(1 - keep_prob))(output)
     model = k.Model(input, output)
