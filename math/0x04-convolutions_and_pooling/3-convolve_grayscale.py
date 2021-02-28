4fa0058bc0197de3c1e2a89117febaacc0cdcf24
anaruzz <laabidigh@gmail.com>
2021-02-28 06:36:57 +0100

    strided convolution

---
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Script that performs a strided convolution on
+grayscale images with custom padding
+"""
+import numpy as np
+
+
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+    """
+    Returns a numpy.ndarray containing
+    the convolved images
+    """
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    sh, sw = stride
+
+    if type(padding) is tuple:
+        padd_h, padd_w = padding
+    elif padding == 'valid':
+        padd_h, padd_w = 0, 0
+    else:
+        padd_h = (((h - 1) * sh + kh - h) // 2) + 1
+        padd_w = (((w - 1) * sw + kw - w) // 2) + 1
+
+    filter_w = (w - kw + (2 * padd_w)) // sw + 1
+    filter_h = (h - kh + (2 * padd_h)) // sh + 1
+
+    output = np.zeros((m, filter_h, filter_w))
+
+    padded_img = np.pad(
+        array=images,
+        pad_width=((0, 0), (padd_h, padd_h), (padd_w, padd_w)),
+        mode="constant",
+    )
+    for i in range(filter_h):
+        for j in range(filter_w):
+            output[:, i, j] = np.sum(
+                kernel * padded_img[:, i*sh: i*sh+kh,
+                                    j*sw: j*sw+kw], axis=(1, 2))
+    return output
