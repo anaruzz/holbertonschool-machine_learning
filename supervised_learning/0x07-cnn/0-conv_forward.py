5d265f3f7494022783aeedf304495d25286a681f
anaruzz <laabidigh@gmail.com>
2021-03-03 18:23:17 +0100

    forward prop

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -13,20 +13,22 @@ def conv_forward(A_prev, W, b, activation,
     m, h_prev, w_prev, c = A_prev.shape
     kh, kw, c, nc = W.shape
     sh, sw = stride
+
     if padding == "valid":
-        ph = 0
-        pw = 0
+        ph, pw = 0, 0
     elif padding == "same":
-        ph = int(((h_prev - 1) * sh + kh - kh % 2 - h_prev) / 2) + 1
-        pw = int(((w_prev - 1) * sw + kw - kw % 2 - w_prev) / 2) + 1
+        ph = (((h_prev - 1) * sh + kh - kh % 2 - h_prev) / 2) + 1
+        pw = (((w_prev - 1) * sw + kw - kw % 2 - w_prev) / 2) + 1
     else:
         ph, pw = padding
 
-    output = np.zeros((m, (h_prev + 2 * ph), (w_prev + 2 * pw), c))
+    filter_h = h_prev + 2 * ph
+    filter_w = w_prev + 2 * pw
+    output = np.zeros((m, filter_h, filter_w, c))
     output[:, ph:h_prev + ph, pw:w_prev + pw, :] = A_prev.copy()
 
-    nh = np.floor(((h_prev + 2 * ph - kh) / stride[0]) + 1).astype(int)
-    nw = np.floor(((w_prev + 2 * pw - kw) / stride[1]) + 1).astype(int)
+    nh = np.floor(((filter_h - kh) / stride[0]) + 1).astype(int)
+    nw = np.floor(((filter_w - kw) / stride[1]) + 1).astype(int)
     S = np.zeros((m, nh, nw, nc))
     im = np.arange(0, m)
 
