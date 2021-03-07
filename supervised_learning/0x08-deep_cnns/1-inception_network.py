bff8cc4b5c8dd6e1f6ac9544dc429fe1baaf7311
anaruzz <laabidigh@gmail.com>
2021-03-07 22:59:27 +0100

    try

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -41,26 +41,27 @@ def inception_network():
     inception = inception_block(l_max2, [64, 96, 128, 16, 32, 32])
     inception = inception_block(inception, [128, 128, 192, 32, 96, 64])
 
-    lmax3 = K.layers.MaxPooling2D(pool_size=(3, 3),
+    l_max3 = K.layers.MaxPooling2D(pool_size=(3, 3),
                                   strides=(2, 2),
                                   padding='same')(inception)
 
-    inception1 = inception_block(lmax3, [192, 96, 208, 16, 48, 64])
+    inception1 = inception_block(l_max3, [192, 96, 208, 16, 48, 64])
     inception2 = inception_block(inception1, [160, 112, 224, 24, 64, 64])
     inception3 = inception_block(inception1, [128, 128, 256, 24, 64, 64])
     inception4 = inception_block(inception3, [112, 144, 288, 32, 64, 64])
     inception5 = inception_block(inception4, [256, 160, 320, 32, 128, 128])
 
-    lmax4 = K.layers.MaxPooling2D(pool_size=(3, 3),
+    l_max4 = K.layers.MaxPooling2D(pool_size=(3, 3),
                                   strides=(2, 2),
                                   padding='same')(inception5)
-    inception6 = inception_block(lmax4, [256, 160, 320, 32, 128, 128])
+
+    inception6 = inception_block(l_max4, [256, 160, 320, 32, 128, 128])
     inception7 = inception_block(inception6, [384, 192, 384, 48, 128, 128])
 
     avg_l = K.layers.AveragePooling2D(pool_size=(7, 7),
                                       padding='same')(inception7)
 
-    drop = K.layers.Dropout(rate=0.6)(avg_l)
+    drop = K.layers.Dropout(rate=0.4)(avg_l)
 
     Y = K.layers.Dense(units=1000, activation='softmax')(drop)
     model = K.models.Model(inputs=X, outputs=Y)
