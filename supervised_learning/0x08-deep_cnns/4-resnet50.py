43d9623835b50d341cc1edc2256e41dd638dc225
anaruzz <laabidigh@gmail.com>
2021-03-08 13:33:44 +0100

    resnet50

--- supervised_learning/0x08-deep_cnns/4-resnet50.py
+++ supervised_learning/0x08-deep_cnns/4-resnet50.py
@@ -17,37 +17,36 @@ def resnet50():
     conv1 = K.layers.Conv2D(filters=64,
                             kernel_size=(7, 7),
                             strides=(2, 2),
-                            padding='same',
-                            activation='relu')(X)
+                            padding='same')(X)
 
     norm1 = K.layers.BatchNormalization()(conv1)
     act1 = K.layers.Activation('relu')(norm1)
-    pool1 = K.layers.MaxPooling2D(pool_size=(3, 3),
-                                  strides=(2, 2),
-                                  padding='same')(act1)
+    pool1 = K.layers.MaxPool2D(pool_size=(3, 3),
+                               strides=(2, 2),
+                               padding='same')(act1)
 
     proj1 = projection_block(pool1, [64, 64, 256], 1)
     id1 = identity_block(proj1, [64, 64, 256])
-    id2 = identity_block(id1, [64, 64, 256])
+    id1 = identity_block(id1, [64, 64, 256])
 
-    proj2 = projection_block(id2, [128, 128, 512])
-    id3 = identity_block(proj2, [128, 128, 512])
-    id4 = identity_block(id3, [128, 128, 512])
-    id5 = identity_block(id4, [128, 128, 512])
+    proj2 = projection_block(id1, [128, 128, 512])
+    id2 = identity_block(proj2, [128, 128, 512])
+    id2 = identity_block(id2, [128, 128, 512])
+    id2 = identity_block(id2, [128, 128, 512])
 
-    proj3 = projection_block(id5, [256, 256, 1024])
-    id6 = identity_block(proj3, [256, 256, 1024])
-    id7 = identity_block(id6, [256, 256, 1024])
-    id8 = identity_block(id7, [256, 256, 1024])
-    id9 = identity_block(id8, [256, 256, 1024])
-    id10 = identity_block(id9, [256, 256, 1024])
+    proj3 = projection_block(id2, [256, 256, 1024])
+    id3 = identity_block(proj3, [256, 256, 1024])
+    id3 = identity_block(id3, [256, 256, 1024])
+    id3 = identity_block(id3, [256, 256, 1024])
+    id3 = identity_block(id3, [256, 256, 1024])
+    id3 = identity_block(id3, [256, 256, 1024])
 
-    proj4 = projection_block(id10, [512, 512, 2048])
-    id11 = identity_block(proj4, [512, 512, 2048])
-    id12 = identity_block(id11, [512, 512, 2048])
+    proj4 = projection_block(id3, [512, 512, 2048])
+    id4 = identity_block(proj4, [512, 512, 2048])
+    id4 = identity_block(id4, [512, 512, 2048])
 
     avg_l = K.layers.AveragePooling2D(pool_size=(7, 7),
-                                      strides=(1, 1))(id12)
+                                      strides=(1, 1))(id4)
     Y = K.layers.Dense(units=1000, activation='softmax',
                        kernel_initializer=kernel)(avg_l)
     model = K.models.Model(inputs=X, outputs=Y)
