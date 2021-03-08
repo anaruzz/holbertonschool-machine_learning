75dc78c5123e4d41ae1457f69d33c4a48dc79480
anaruzz <laabidigh@gmail.com>
2021-03-08 10:56:02 +0100

    identity block fix

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -14,27 +14,28 @@ def identity_block(A_prev, filters):
     kernel = K.initializers.he_normal()
 
     layer1x1 = K.layers.Conv2D(filters=F11,
-                           kernel_size=(1, 1),
-                           padding='same',
-                           kernel_initializer=kernel)(A_prev)
+                               kernel_size=(1, 1),
+                               padding='same',
+                               kernel_initializer=kernel)(A_prev)
 
     layer1x1 = K.layers.BatchNormalization()(layer1x1)
 
     layer1x1 = K.layers.Activation('relu')(layer1x1)
 
     layer3x3 = K.layers.Conv2D(filters=F3,
-                             kernel_size=(3, 3),
-                             padding='same',
-                             kernel_initializer=kernel)(layer1x1)
+                               kernel_size=(3, 3),
+                               padding='same',
+                               kernel_initializer=kernel)(layer1x1)
 
     layer3x3 = K.layers.BatchNormalization()(layer3x3)
     layer3x3 = K.layers.Activation('relu')(layer3x3)
 
     layer1x1 = K.layers.Conv2D(filters=F12,
-                             kernel_size=(1, 1),
-                             padding='same',
-                             kernel_initializer=kernel)(layer3x3)
+                               kernel_size=(1, 1),
+                               padding='same',
+                               kernel_initializer=kernel)(layer3x3)
 
+    layer1x1 = K.layers.BatchNormalization()(layer1x1)
     output = K.layers.Add()([layer1x1, A_prev])
 
     output = K.layers.Activation('relu')(output)
