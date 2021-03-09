c981daf37740458d5cbd0ea12b7081b4cb2b5ae6
anaruzz <laabidigh@gmail.com>
2021-03-09 14:26:54 +0100

    densenet121

--- supervised_learning/0x08-deep_cnns/7-densenet121.py
+++ supervised_learning/0x08-deep_cnns/7-densenet121.py
@@ -14,27 +14,33 @@ def densenet121(growth_rate=32, compression=1.0):
     kernel = K.initializers.he_normal()
     X = K.Input(shape=(224, 224, 3))
 
-
     norm1 = K.layers.BatchNormalization()(X)
     act1 = K.layers.Activation('relu')(norm1)
     conv1 = K.layers.Conv2D(filters=2*growth_rate,
                             kernel_size=(7, 7),
                             padding='same',
+                            strides=(2, 2),
                             kernel_initializer=kernel)(act1)
     max_p = K.layers.MaxPooling2D(pool_size=(3, 3),
                                   strides=(2, 2),
                                   padding='same')(conv1)
-    dense1, nbf1 = dense_block(max_p, 2*growth_rate, growth_rate, 6)
+
+    nb_filters = max_p.shape[-1].value
+
+    dense1, nbf1 = dense_block(max_p, nb_filters, growth_rate, 6)
     trans1, nbf2 = transition_layer(dense1, nbf1, compression)
+
     dense2, nbf3 = dense_block(trans1, nbf2, growth_rate, 12)
     trans2, nbf4 = transition_layer(dense2, nbf3, compression)
+
     dense3, nbf5 = dense_block(trans2, nbf4, growth_rate, 24)
     trans3, nbf6 = transition_layer(dense3, nbf5, compression)
+
     dense4, nbf7 = dense_block(trans3, nbf6, growth_rate, 16)
     avg = K.layers.AveragePooling2D(pool_size=(7, 7),
                                     strides=(1, 1))(dense4)
     Y = K.layers.Dense(units=1000,
-                            activation="softmax",
-                            kernel_initializer=kernel)(avg)
+                       activation="softmax",
+                       kernel_initializer=kernel)(avg)
     model = K.models.Model(inputs=X, outputs=Y)
     return model
