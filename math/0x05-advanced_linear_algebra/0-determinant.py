3c7f03f40a95d9ce9d91850c3d070b73c1ce3762
anaruzz <laabidigh@gmail.com>
2021-03-18 19:06:22 +0100

    fml

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -17,18 +17,18 @@ def determinant(matrix):
     """
     if matrix == [[]]:
         return 1
-    if len(matrix) == 1:
-        return matrix[0][0]
-
-    if type(matrix) is not list or matrix == []:
-        raise TypeError('matrix must be a list of lists')
+    if type(matrix) != list or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
 
     for i in matrix:
         if type(i) != list:
             raise TypeError("matrix must be a list of lists")
         if len(matrix) != len(i):
             raise ValueError("matrix must be a square matrix")
-            
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+
     if len(matrix) == 2:
         det = matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
     else:
