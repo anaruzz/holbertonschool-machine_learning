89bd63b21f6b64de9cc3b603d3a595ecef6e78ae
anaruzz <laabidigh@gmail.com>
2021-03-24 22:30:09 +0100

    pca

---
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+A function that perfors PCA on a dataset
+"""
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """
+    Returns the weights matrix, W that maintains
+    var fraction of X's original variance
+    """
+    _, s, v = np.linalg.svd(X)
+    n = 0
+    sv = s.sum() * var
+    t = s[0]
+    while (t < sv):
+        t += s[n]
+        n += 1
+
+    return v[:n+1].T
