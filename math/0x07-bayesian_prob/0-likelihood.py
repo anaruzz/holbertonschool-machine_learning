077963f7e476877807db995ec253af07a4bee0cc
anaruzz <laabidigh@gmail.com>
2021-03-26 17:12:52 +0100

    likelihood fix

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -16,13 +16,16 @@ def likelihood(x, n, P):
     if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
     if type(x) is not int or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError("x must be an integer that is greater\
+                          than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
     if type(P) is not np.ndarray or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     if np.any([v < 0 or v > 1 for v in P]):
         raise ValueError("All values in P must be in the range [0, 1]")
 
     f = np.math.factorial
-    combination = f(n) / (f(x) * f (n - x))
+    combination = f(n) / (f(x) * f(n - x))
     prob = np.power(P, x) * np.power(1 - P, n - x)
-    return combination * prob 
+    return combination * prob
