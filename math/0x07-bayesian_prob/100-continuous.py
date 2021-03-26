37b24e7c72b0650908adf01121b54d2a18a36f85
anaruzz <laabidigh@gmail.com>
2021-03-26 20:21:22 +0100

    beta

--- math/0x07-bayesian_prob/100-continuous.py
+++ math/0x07-bayesian_prob/100-continuous.py
@@ -5,6 +5,7 @@ probability of developing severe side effects falls
 within a specific range given the data
 """
 import numpy as np
+from scipy import special
 
 
 def posterior(x, n, p1, p2):
@@ -19,11 +20,15 @@ def posterior(x, n, p1, p2):
         raise ValueError(err)
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if type(p1) is not float or p1 < 0 or p1 < 1:
+    if type(p1) is not float or p1 < 0 or p1 > 1:
         raise ValueError("p1 must be a float in the range [0, 1]")
-    if type(p2) is not float or p2 < 0 or p2 < 1:
+    if type(p2) is not float or p2 < 0 or p2 > 1:
         raise ValueError("p2 must be a float in the range [0, 1]")
     if p2 <= p1:
         raise ValueError("p2 must be greater than p1")
 
-    return None
+    f1 = x + 1
+    f2 = n - x + 2
+    alfa = special.btdtr(f1, f2, p2)
+    beta = special.btdtr(f1, f2, p1)
+    return alfa - beta
