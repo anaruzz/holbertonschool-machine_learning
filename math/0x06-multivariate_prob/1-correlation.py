3ca80c774ec577fb1ec76e074698d6a5407fa5d9
anaruzz <laabidigh@gmail.com>
2021-03-26 12:11:25 +0100

    correlation

---
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+A function that calculates the correlation of a data set
+"""
+import numpy as np
+
+
+def correlation(C):
+    """
+    Returns numpy array containing the correlation
+    matrix
+    """
+    if type(C) is not np.ndarray:
+        raise TypeError("X must be a numpy.ndarray")
+
+    if C.shape[0] != C.shape[1]:
+        raise ValueError("C must be a 2D square matrix")
+
+    var = np.sqrt(np.diag(C))
+    out = np.outer(var, var)
+    cor = C / out
+
+    return cor
