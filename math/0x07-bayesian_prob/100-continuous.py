fc09613836b69c1d17876d69b306e22afb8dcfc0
anaruzz <laabidigh@gmail.com>
2021-03-26 20:30:04 +0100

    test

--- math/0x07-bayesian_prob/100-continuous.py
+++ math/0x07-bayesian_prob/100-continuous.py
@@ -13,12 +13,12 @@ def posterior(x, n, p1, p2):
     Returns the posterior probability that p is within
     the range [p1, p2] given x and n
     """
-    if type(n) is not int or n <= 0:
+    if type(n) is not int or n < 1:
         raise ValueError("n must be a positive integer")
     if type(x) is not int or x < 0:
         err = "x must be an integer that is greater than or equal to 0"
         raise ValueError(err)
-    if x >= n:
+    if x > n:
         raise ValueError("x cannot be greater than n")
     if type(p1) is not float or p1 < 0 or p1 > 1:
         raise ValueError("p1 must be a float in the range [0, 1]")
