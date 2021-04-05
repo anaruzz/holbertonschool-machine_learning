359312fd97acdf5a6c4cc8913d746381f098a03e
anaruzz <laabidigh@gmail.com>
2021-04-05 18:31:58 +0100

    initialize cluter centroids

---
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Function that initializes cluster centroids for K-means
+"""
+import numpy as np
+
+
+def initialize(X, k):
+    """
+    Returns a numpy.ndarray of shape (k, d) containing
+    the initialized centroids for each cluster,
+    or None on failure
+    """
+    if type(X) is not np.ndarray:
+        return None
+    if type(k) is not int or k <= 0:
+        return None
+
+    _, d = X.shape
+    min = X.min(axis=0)
+    max = X.max(axis=0)
+    values = np.random.uniform(min, max, (k, d))
+
+    return values
