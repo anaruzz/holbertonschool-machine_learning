98946ab3b6c2cb45a7b1bebca87fcb36eb4db013
anaruzz <laabidigh@gmail.com>
2021-04-05 20:44:15 +0100

    initialize gmm pep8 fix

--- unsupervised_learning/0x01-clustering/4-initialize.py
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -19,7 +19,7 @@ def initialize(X, k):
     _, d = X.shape
     m, _ = kmeans(X, k)
     if (m is None):
-            return None, None, None
+        return None, None, None
     p = (1 / k) * np.ones(k)
     s = np.ones((k, d, d)) * np.eye(d)
     return p, m, s
