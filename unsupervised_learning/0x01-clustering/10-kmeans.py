def96140552176b46374c2b94980194a5ebcc121
anaruzz <laabidigh@gmail.com>
2021-05-01 02:46:37 +0100

    pep8 fix

--- unsupervised_learning/0x01-clustering/10-kmeans.py
+++ unsupervised_learning/0x01-clustering/10-kmeans.py
@@ -8,8 +8,6 @@ import sklearn.cluster
 def kmeans(X, k):
     """
     Returns: C, clss
-    C is a numpy.ndarray of shape (k, d) containing the centroid means for each cluster
-    clss is a numpy.ndarray of shape (n,) containing the index of the cluster in C that each data point belongs to
     """
     model = sklearn.cluster.KMeans(n_clusters=k).fit(X)
     clss = model.labels_
