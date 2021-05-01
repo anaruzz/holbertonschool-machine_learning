f8baa24a10c6a509a1e72257bb2d0c586b07c3af
anaruzz <laabidigh@gmail.com>
2021-05-01 02:36:25 +0100

    hope it's last fix

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -34,15 +34,15 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
         prev = likelihood
         if verbose and (i % 10 == 0):
             print("Log Likelihood after {} iterations: {}".
-                   format(i, likelihood.round(5)))
+                  format(i, likelihood.round(5)))
 
         pi, m, S = maximization(X, g)
         g, likelihood = expectation(X, pi, m, S)
         if abs(likelihood - prev) <= tol:
             break
-            
+
     if verbose:
         print("log likelihood after {} iterations: {}"
-               .format(i+1, likelihood.round(5)))
+              .format(i+1, likelihood.round(5)))
 
     return pi, m, S, g, likelihood
