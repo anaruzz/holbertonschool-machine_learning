95a49eb883fcec58d81756696d29749e13e215ac
anaruzz <laabidigh@gmail.com>
2021-05-17 00:29:15 +0100

    forward algorithm

--- unsupervised_learning/0x02-hmm/3-forward.py
+++ unsupervised_learning/0x02-hmm/3-forward.py
@@ -11,24 +11,47 @@ def forward(Observation, Emission, Transition, Initial):
     Returns: P, F, or None, None on failure
 
     P is the likelihood of the observations given the model
-    F is a numpy.ndarray of shape (N, T) containing the forward path probabilities
-        F[i, j] is the probability of being in hidden state i at time j given the previous observations
+    F is a numpy.ndarray of shape (N, T) containing the forward
+    path probabilities
+    F[i, j] is the probability of being in hidden state i at time j
+    given the previous observations
     """
-    if type(P) is not np.ndarray or len(P.shape) != 2:
-        return None
+    if type(Observation) is not np.ndarray or len(Observation.shape) != 1:
 
-    n, m = P.shape
-    if n != m:
+        return None, None
 
-        return None
+    if type(Emission) is not np.ndarray or len(Emission.shape) != 2:
+        return None, None
 
-    if np.any(P < 0):
-        return None
+    if type(Transition) is not np.ndarray or len(Transition.shape) != 2:
 
-    if not np.all(np.isclose(P.sum(axis=1), 1)):
-        return None
+        return None, None
 
-    for i in range(n):
-        if P[i, i] == 1:
-            return True
-    return False
+    if type(Initial) is not np.ndarray or len(Initial.shape) != 2:
+        return None, None
+
+    test = np.sum(Emission, axis=1)
+    if not (test == 1).all():
+        return None, None
+
+    test = np.sum(Transition, axis=1)
+    if not (test == 1).all():
+        return None, None
+
+    test = np.sum(Initial, axis=0)
+    if not (test == 1).all():
+        return None, None
+
+    T = Observation.shape[0]
+    F = np.zeros((Initial.shape[0], T))
+    index = Observation[0]
+    Emission_ind = Emission[:, index]
+    F[:, 0] = Initial.T * Emission_ind
+
+    for j in range(1, T):
+        for i in range(Initial.shape[0]):
+            F[i, j] = np.sum(Emission[i, Observation[j]]
+                             * Transition[:, i] * F[:, j - 1], axis=0)
+
+    P = np.sum(F[:, T-1:], axis=0)[0]
+    return P, F
