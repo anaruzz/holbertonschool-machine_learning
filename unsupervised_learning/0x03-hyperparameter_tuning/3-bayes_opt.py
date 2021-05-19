8712506964be3b8ee56e0125d8864b8c3a9d596a
anaruzz <laabidigh@gmail.com>
2021-05-19 01:03:29 +0100

    update

--- unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
@@ -10,15 +10,14 @@ class BayesianOptimization():
     """
     A class that represents a Bayesian Optimization
     """
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
+                 l=1, sigma_f=1, xsi=0.01, minimize=True):
         """
         constructor for class
         """
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
-        self.X_s = np.zeros((ac_samples, 1))
-        self.X_s = np.linspace(start=bounds[0], stop=bounds[0],
-                               num=ac_samples, endpoint=True)
-        self.X_s = self.X_s.reshape(ac_samples, 1)
+        min, max = bounds
+        self.X_s = np.linspace(min, max, ac_samples).reshape(-1, 1)
         self.xsi = xsi
         self.minimize = minimize
