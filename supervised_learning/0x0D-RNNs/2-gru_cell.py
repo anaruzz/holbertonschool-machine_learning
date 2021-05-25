523ec0025fdcc815cea8f412c874ba9575be0117
anaruzz <laabidigh@gmail.com>
2021-05-25 15:34:53 +0100

    GRU Cell

---
+++ supervised_learning/0x0D-RNNs/2-gru_cell.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Script that represents a gated recurrent unit
+"""
+import numpy as np
+
+
+class GRUCell():
+    """
+    Class that represents a gated recurrent unit
+    """
+    def __init__(self, i, h, o):
+        """
+        class constructor
+        """
+        self.Wz = np.random.normal(size=(i + h, h))
+        self.Wr = np.random.normal(size=(i + h, h))
+        self.Wh = np.random.normal(size=(i + h, h))
+        self.Wy = np.random.normal(size=(h, o))
+        self.bz = np.zeros((1, h))
+        self.br = np.zeros((1, h))
+        self.bh = np.zeros((1, h))
+        self.by = np.zeros((1, h))
+
+    def forward(self, h_prev, x_t):
+        """
+        Performs forward propagation for one step
+        Returns h_next: the next hidden state
+                y: the output of the cell
+        """
+        # Concat h_prev and x_t to match Wh dimensions
+        x = np.concatenate((h_prev, x_t), axis=1)
+
+        z = np.dot(x, self.Wz) + self.bz
+        # activating usng sigmoid
+        z = 1 / (1 + np.exp(-z))
+
+        r = np.dot(x, self.Wr) + self.br
+        # activating usng sigmoid
+        r = 1 / (1 + np.exp(-r))
+
+        x = np.concatenate((r * h_prev, x_t), axis=1)
+        h = np.tanh(np.dot(x, self.Wh) + self.bh)
+
+        h_t = z * h + (1 - z) * h_prev
+
+        y = np.dot(h_t, self.Wy) + self.by
+        # activating usng softmax
+        y = np.exp(y) / np.sum(np.exp(y), axis=1, keepdims=True)
+
+        return h_t, y
