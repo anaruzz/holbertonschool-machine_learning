ebf6bc57ee0c0cf122e46008917266a91dfa4d6a
anaruzz <laabidigh@gmail.com>
2021-06-07 19:32:30 +0100

    keras embedding

---
+++ supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Script hat converts a gensim word2vec model to a
+keras Embedding layer
+"""
+# import tensorflow.keras as K
+
+
+def gensim_to_keras(model):
+    """
+    Returns: the trainable keras Embedding
+    """
+    k_embedding =  model.wv.get_keras_embedding(train_embeddings=True)
+    return k_embedding
