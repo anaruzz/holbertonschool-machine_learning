048f207e7abd5c6c8fcbd6a18131f472b07b8c54
anaruzz <laabidigh@gmail.com>
2021-06-08 04:11:29 +0100

    try

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -12,36 +12,29 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     """
     returns: pi, m, S, g, l, or None, None, None on failure
     """
-    if type(X) is not np.ndarray or len(X.shape) != 2:
-        return None, None, None, None, Non
-
-    if type(k) is not int or k < 1:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None, None
-
-    if type(tol) is not float or tol < 0:
+    if not isinstance(k, np.int) or k < 1:
         return None, None, None, None, None
-
-    if type(verbose) is not bool:
+    if not isinstance(tol, float) or tol < 0:
         return None, None, None, None, None
-
-    if type(iterations) is not int or iterations < 1:
+    if not isinstance(verbose, bool):
+        return None, None, None, None, None
+    if not isinstance(iterations, int) or iterations < 1:
         return None, None, None, None, None
 
     pi, m, S = initialize(X, k)
     g, likelihood = expectation(X, pi, m, S)
-
     for i in range(iterations):
-        prev = likelihood
+        prev_likelihood = likelihood
         if verbose and (i % 10) == 0:
             print('Log Likelihood after {} iterations: {}'.format(
                 i, likelihood.round(5)))
         pi, m, S = maximization(X, g)
         g, likelihood = expectation(X, pi, m, S)
-        if abs(likelihood - prev) <= tol:
+        if abs(likelihood - prev_likelihood) <= tol:
             break
-
     if verbose:
-        print("log likelihood after {} iterations: {}"
-              .format(i + 1, likelihood.round(5)))
-
+        print('Log Likelihood after {} iterations: {}'.format(
+            i + 1, likelihood.round(5)))
     return pi, m, S, g, likelihood
