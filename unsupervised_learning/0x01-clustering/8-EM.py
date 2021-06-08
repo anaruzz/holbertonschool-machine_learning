ff47fc16d6e9b3f7c9e76b8af78fd23f73f30842
anaruzz <laabidigh@gmail.com>
2021-06-08 04:05:06 +0100

    EM

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -13,33 +13,32 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     returns: pi, m, S, g, l, or None, None, None on failure
     """
     if type(X) is not np.ndarray or len(X.shape) != 2:
-        return None, None, None, None, None
+        return None
 
     if type(k) is not int or k < 1:
-        return None, None, None, None, None
+        return None
 
     if type(tol) is not float or tol < 0:
-        return None, None, None, None, None
+        return None
 
     if type(verbose) is not bool:
-        return None, None, None, None, None
+        return None
 
     if type(iterations) is not int or iterations < 1:
-        return None, None, None, None, None
+        return None
 
     pi, m, S = initialize(X, k)
     g, likelihood = expectation(X, pi, m, S)
-    prev = 0
+
     for i in range(iterations):
-        if abs(likelihood - prev) <= tol:
-            break
         prev = likelihood
         if verbose and (i % 10) == 0:
-            print("Log Likelihood after {} iterations: {}".
-                  format(i, likelihood.round(5)))
-
+            print('Log Likelihood after {} iterations: {}'.format(
+                i, likelihood.round(5)))
         pi, m, S = maximization(X, g)
         g, likelihood = expectation(X, pi, m, S)
+        if abs(likelihood - prev) <= tol:
+            break
 
     if verbose:
         print("log likelihood after {} iterations: {}"
