e29f5ecce30637462ce7b4241fa7297507b542e3
anaruzz <laabidigh@gmail.com>
2021-06-17 23:21:30 +0100

    ok

--- unsupervised_learning/0x02-hmm/6-baum_welch.py
+++ unsupervised_learning/0x02-hmm/6-baum_welch.py
@@ -5,14 +5,14 @@ hidden markov model
 import numpy as np
 
 
-def baum_welch(Obs, Transition, Emission, Init, iterations=1000):
+def baum_welch(Obs, Trans, Emiss, Init, iterations=1000):
     """
-    Returns: the converged Transitionition, Emissionion, or None, None on failure
+    Returns: the converged Transition, Emission, or None, None on failure
     """
     T = Obs.shape[0]
-    N, M = Emission.shape
+    N, M = Emiss.shape
 
-    def forward(Obs, Transition, Emission, Init):
+    def forward(Obs, Trans, Emiss, Init):
         """
         calculates the most likely sequence of hidden states for a
         hidden markov model
@@ -20,12 +20,12 @@ def baum_welch(Obs, Transition, Emission, Init, iterations=1000):
         alpha = np.ndarray((N, T))
         state = Init[:, 0]
         for t in range(T):
-            state = state * Emission[:, Obs[t]]
+            state = state * Emiss[:, Obs[t]]
             alpha[:, t] = state
-            state = np.matmul(Transition.T, state)
+            state = np.matmul(Trans.T, state)
         return alpha[:, -1].sum(), alpha
 
-    def backward(Obs, Transition, Emission, Init):
+    def backward(Obs, Trans, Emiss, Init):
         """
         performs the backward algorithm for a hidden markov model
         """
@@ -33,32 +33,32 @@ def baum_welch(Obs, Transition, Emission, Init, iterations=1000):
         state = np.asarray([1] * N)
         beta[:, -1] = state
         for t in range(T - 2, -1, -1):
-            state = np.matmul(Transition, state * Emission[:, Obs[t + 1]])
+            state = np.matmul(Trans, state * Emiss[:, Obs[t + 1]])
             beta[:, t] = state
         return (beta[:, 0] * Init[:, 0]
-                * Emission[:, Obs[0]]).sum(), beta
+                * Emiss[:, Obs[0]]).sum(), beta
     a = None
     while iterations:
         iterations -= 1
-        P, alpha = forward(Obs, Transition, Emission, Init)
-        P2, beta = backward(Obs, Transition, Emission, Init)
+        P, alpha = forward(Obs, Trans, Emiss, Init)
+        P2, beta = backward(Obs, Trans, Emiss, Init)
 
         forbackxi = alpha[:, None, :-1] * beta[None, :, 1:]
-        emittedprobs = Emission[:, Obs[1:]]
-        xi = forbackxi * Transition[..., None] * emittedprobs[None, :, ...]
+        emittedprobs = Emiss[:, Obs[1:]]
+        xi = forbackxi * Trans[..., None] * emittedprobs[None, :, ...]
         xi /= xi.sum(axis=(0, 1))
         forbackga = alpha * beta
         gamma = forbackga / forbackga.sum(axis=0)
-        Transition = xi.sum(axis=2) / xi.sum(axis=(1, 2))
-        Transition = Transition.T
+        Trans = xi.sum(axis=2) / xi.sum(axis=(1, 2))
+        Trans = Trans.T
         for emit in range(M):
             gammanum = gamma[:, Obs == emit]
-            Emission[:, emit] = gammanum.sum(axis=1) / gamma.sum(axis=1)
-        if ((np.all(a == Transition)
-             and np.all(emprev == Emission)
+            Emiss[:, emit] = gammanum.sum(axis=1) / gamma.sum(axis=1)
+        if ((np.all(a == Trans)
+             and np.all(emprev == Emiss)
              and np.all(initprev == Init))):
-            return Transition, Emission
-        a = Transition
+            return Trans, Emiss
+        a = Trans
         initprev = Init
-        emprev = Emission
-    return Transition, Emission
+        emprev = Emiss
+    return Trans, Emiss
