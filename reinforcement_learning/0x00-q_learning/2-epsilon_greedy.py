54e7d582a19d070ffd0cb652c39338eeeaafbd26
anaruzz <laabidigh@gmail.com>
2021-06-30 15:38:51 +0100

    epsilon greedy function

---
+++ reinforcement_learning/0x00-q_learning/2-epsilon_greedy.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Script  that uses epsilon-greedy to determine
+the next action
+"""
+import numpy as np
+
+
+def epsilon_greedy(Q, state, epsilon):
+    """
+    Returns: the next action indexf
+    """
+    n = np.random.uniform(0, 1)
+    if n < epsilon:
+        return np.argmax(Q[state, :])
+    else:
+        return np.random.randint(Q.shape[1])
