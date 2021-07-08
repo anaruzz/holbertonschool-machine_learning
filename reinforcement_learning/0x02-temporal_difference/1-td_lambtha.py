57d4265c011375844712c3dd1260844fac446393
anaruzz <laabidigh@gmail.com>
2021-07-08 23:43:07 +0100

    delta

---
+++ reinforcement_learning/0x02-temporal_difference/1-td_lambtha.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Script that performs the TD(λ) algorithm
+"""
+import gym
+import numpy as np
+
+
+def td_lambtha(env, V, policy, lambtha, episodes=5000,
+               max_steps=100, alpha=0.1, gamma=0.99):
+    """
+    Returns: V
+    V: the updated value estimate
+    """
+    state_d = env.observation_space.n
+    Et = np.zeros(state_d)
+    for _ in range(episodes):
+        state = env.reset()
+        for _ in range(max_steps):
+            Et *= lambtha * gamma
+            Et[state] += 1.0
+            a = policy(state)
+            new_state, reward, done, info = env.step(a)
+            if env.desc.reshape(state_d)[new_state] == b'G':
+                reward = 1
+            if env.desc.reshape(state_d)[new_state] == b'H':
+                reward = -1
+            delta = reward + gamma * V[new_state] - V[state]
+            V[state] = V[state] + alpha * delta * Et[state]
+            if done:
+                break
+            state = new_state
+    return V
