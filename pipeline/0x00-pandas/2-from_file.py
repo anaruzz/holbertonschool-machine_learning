11c8637cecb9d53bfe7d08130e9dcfacbac38b70
anaruzz <laabidigh@gmail.com>
2021-07-22 18:08:09 +0100

    load data from file

--- pipeline/0x00-pandas/2-from_file.py
+++ pipeline/0x00-pandas/2-from_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+A Function that creates a pd.DataFrame from a file
+"""
+import pandas as pd
+
+
+def from_file(filename, delimiter):
+    """
+    Returns the loaded pd.DataFrame
+    """
+    return pd.read_csv(filename, sep=delimiter)
