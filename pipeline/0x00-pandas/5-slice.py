76573e4e87cd1103926072caf31e345e13283ecb
anaruzz <laabidigh@gmail.com>
2021-08-20 12:15:49 +0100

    files

--- pipeline/0x00-pandas/5-slice.py
+++ pipeline/0x00-pandas/5-slice.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""
+A script that slices the pd.dataframe
+"""
+
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+df = df[["High", "Low", "Close", "Volume_(BTC)"]][::60]
+
+print(df.tail())
