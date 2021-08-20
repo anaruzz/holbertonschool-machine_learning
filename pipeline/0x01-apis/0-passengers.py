eabccb6e23cceb98b45133e7464fc571ada72f0c
anaruzz <laabidigh@gmail.com>
2021-08-20 16:45:07 +0100

    Can i join?

---
+++ pipeline/0x01-apis/0-passengers.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+A method that returns the list of ships that 
+can hold a given number of passengers from SWAPI API
+"""
+import requests
+
+
+def availableShips(passengerCount):
+    """
+    returns the list of ships that can hold a given
+    number of passengers
+    """
+    url = "https://swapi-api.hbtn.io/api/starships"
+    req = requests.get(url)
+    json = req.json()
+    ships = []
+    while req.status_code == 200:
+        content = json["results"]
+        for ship in content:
+            if (ship["passengers"] >= str(passengerCount)):
+                ships.append(ship["name"])
+        
+        url = json["next"]
+        if (url is not None):
+            req = requests.get(url)
+            json = req.json()
+        else:
+            break
+    return ships
+
