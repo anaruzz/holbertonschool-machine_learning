73f7bc53a1c6dd5c69304fc6b7376ea1f62e3693
anaruzz <laabidigh@gmail.com>
2021-08-21 16:15:51 +0100

    Github API location

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -2,12 +2,16 @@
 """
 A script that prints the location of a specific user from github API
 """
-from datetime import datetime
+
 import requests
 import sys
+import time
 
 
 if __name__ == '__main__':
+    """
+    Return location or Error or time
+    """
     url = sys.argv[1]
     req = requests.get(url)
 
@@ -15,9 +19,8 @@ if __name__ == '__main__':
         print("Not found")
     elif req.status_code == 403:
         limit = req.headers['X-Ratelimit-Reset']
-        now = datetime.now().timestamp()
-        diff = (int(limit) - int(now)) / 60
-        print('Reset in {} min'.format(int(diff)))
+        x = (int(limit) - int(time.time())) / 60
+        print('Reset in {} min'.format(int(x)))
     elif req.status_code == 200:
         json = req.json()
         print(json["location"])
